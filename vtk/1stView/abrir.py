323d62c95c1c8c56ec0110eb977cb81888acf1b0
Gwerr1002 <98936739+Gwerr1002@users.noreply.github.com>
2022-10-30 01:27:34 -0500

    Visualizaciones de archivos DICOM
    agregué scripts de python para abrir archivos DICOM con vtk

---
+++ vtk/1stView/abrir.py
@@ -0,0 +1,44 @@
+from vtk import vtkDICOMImageReader as dcmReader
+from vtk import *
+
+pathimage = "..\..\Volumenes\IMG_20211012_18.4dv\IMAGE\Y2018M12\D10\E0000001\I0000034.dcm"
+source = dcmReader()
+#leer DICOM, tambien se puede leer directorio con SetDirectoryName
+source.SetFileName(pathimage)
+source.Update()
+
+#imageData = source.GetOutput()
+
+colors = vtkNamedColors()
+print(source.GetOutput().GetScalarTypeAsString())
+
+castFilter = vtkImageCast()
+castFilter.SetInputConnection(source.GetOutputPort())
+castFilter.SetOutputScalarTypeToUnsignedChar()
+castFilter.Update()
+
+# Create an actor
+actor = vtkImageActor()
+actor.GetMapper().SetInputConnection(castFilter.GetOutputPort())
+
+# Setup renderer
+renderer = vtkRenderer()
+renderer.AddActor(actor)
+#renderer.SetBackground(colors.GetColor3d('DarkSlateGra'))
+renderer.ResetCamera()
+
+# Setup render window
+renderWindow = vtkRenderWindow()
+renderWindow.AddRenderer(renderer)
+renderWindow.SetWindowName('Cast')
+
+# Setup render window interactor
+renderWindowInteractor = vtkRenderWindowInteractor()
+style = vtkInteractorStyleImage()
+renderWindowInteractor.SetInteractorStyle(style)
+
+# Render and start interaction
+renderWindowInteractor.SetRenderWindow(renderWindow)
+renderWindow.Render()
+renderWindowInteractor.Initialize()
+renderWindowInteractor.Start()
